901ddff1f464349a55d8871ca6e0164fa19641a1
Pratap Lakshman <pratapl@microsoft.com>
2018-09-03 17:16:17 +0530

    Minor cleanup.

--- Dice/2/src/main/java/App.java
+++ Dice/2/src/main/java/App.java
@@ -1,26 +1,26 @@
 public class App
 {
-  public static int roll(Dice d)
-  {
-    java.util.Random r = new java.util.Random();
-    return (1 + r.nextInt(d.sides()));
-  }
-
-
-  public static void main(String [] args)
-  {
-    // create a 6 sided die
-    Dice d1 = new Dice(6);
-    for (int i = 0; i < 10; i++)
+    public static int roll(Dice d)
     {
-      System.out.println(roll(d1));
+        java.util.Random r = new java.util.Random();
+        return (1 + r.nextInt(d.sides()));
     }
 
-    // create an 8 sided die
-    Dice d2 = new Dice(8);
-    for (int i = 0; i < 10; i++)
+
+    public static void main(String [] args)
     {
-      System.out.println(roll(d2));
+        // create a 6 sided die
+        Dice d1 = new Dice(6);
+        for (int i = 0; i < 10; i++)
+        {
+            System.out.println(roll(d1));
+        }
+
+        // create an 8 sided die
+        Dice d2 = new Dice(8);
+        for (int i = 0; i < 10; i++)
+        {
+            System.out.println(roll(d2));
+        }
     }
-  }
 }
